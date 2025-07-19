aea783942662903ef7b518ca8bb75392af21f454
Yash Kishor Maske <yashmaske1235@outlook.com>
2025-07-19 20:05:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
      origin: [
     "http://localhost:5173",
-    "https://flatmates-confesion-15wz.vercel.app"
+    "https://electricity-bill-4ov1.vercel.app"
   ]
 }));
 
