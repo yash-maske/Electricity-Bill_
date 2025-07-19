849976dff7bb6f9505a7d57baf378f679c947f54
Yash Kishor Maske <yashmaske1235@outlook.com>
2025-07-19 20:27:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://electricity-bill-sun3aopb2-yash-maskes-projects-93f4ac16.vercel.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 const mongoURI = process.env.MONGODB_URI;
